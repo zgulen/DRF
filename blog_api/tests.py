a965503752f17a8f7bf2472fe800525a0e105d3f
zgulen <gulenzubeyir@gmail.com>
2022-11-21 18:33:02 +0300

    ready to deploy

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class PostTest(APITestCase):
+    def test_view_post(self):
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
