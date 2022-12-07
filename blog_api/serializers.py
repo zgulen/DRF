9ef6a5e0b06c2bbd32515becd7a0d44a6adb88be
zgulen <gulenzubeyir@gmail.com>
2022-12-07 17:57:59 +0300

    a

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from blog.models import Post
-from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
